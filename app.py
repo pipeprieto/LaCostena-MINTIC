69bc3638139b0f315ca0b5f4dea4960ec8245703
Andrés Prieto <pipeprieto0902@gmail.com>
2021-10-21 15:12:19 -0500

    Nuevos cambios

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from logging import debug
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -15,3 +16,5 @@ def registro():
 def index():
     return render_template('index.html')
 
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
