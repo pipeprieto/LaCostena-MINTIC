534c3a8776c401394ee3aa5142d56ff9ecb7d4a3
Andrés Prieto <pipeprieto0902@gmail.com>
2021-10-21 21:11:55 -0500

    Intento De Conexión Realizada

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from logging import debug
-from flask import Flask, render_template
-
+from flask import Flask, render_template,request,redirect
+from flask_mysqldb import MySQL
 app = Flask(__name__)
+
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']='Pipe1709_'
+app.config['MYSQL_DB']='Lacosteña'
+mysql=MySQL(app)
+
+
+
 
 @app.route("/")
 def login():
@@ -11,6 +20,21 @@ def login():
 @app.route("/Registro")
 def registro():
     return render_template("Registro.html")
+
+@app.route("/Crear", methods=["POST"])
+def create():
+    if request.method == "POST":
+        fullname = request.form['fullname']
+        correo = request.form['correo']
+        password = request.form['password']
+        rptpassword=request.form['rptpassword']
+        if rptpassword == password :
+            cur= mysql.connection.cursor()
+            cur.execute("INSERT INTO empleado (fullname,correo,password) VALUES (%s,%s,%s)",(fullname,correo,password))
+            mysql.connection.commit()
+            return redirect("/")
+        else:
+            return "Error grave"
 
 @app.route("/home")
 def index():
