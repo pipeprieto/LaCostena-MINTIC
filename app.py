2bc1ffe5b2af2227fd5c4250dd067453de436e3e
Andrés Prieto <pipeprieto0902@gmail.com>
2021-10-27 20:07:37 -0500

    data is shown on index.html table

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from logging import debug
 from flask import Flask, render_template,request,redirect,session   
-from flask_mysqldb import MySQL,MySQLdb
+# from flask_mysqldb import MySQL,MySQLdb
 from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql=MySQL()
@@ -14,38 +14,46 @@ mysql.init_app(app)
 
 @app.route("/")
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        connection = mysql.connect()
+        cur = connection.cursor()
+        cur.execute("SELECT * FROM `empleado`")
+        results = cur.fetchall()
+        print(results)
+        connection.commit()
+        return render_template("login.html",results=results)
 
 
 @app.route("/Registro")
 def registro():
-    return render_template("Registro.html")
+    return render_template('Registro.html')
 
 @app.route("/Crear", methods=["GET","POST"])
 def create():
-    if request.method == "GET":
-        connection = mysql.connect()
-        cur = connection.cursor()
-        cur.execute("SELECT * FROM empleado")
-        result = cur.fetchall()
-    if request.method == "POST":
-        name = request.form['nombre']
-        correo = request.form['correo']
-        password = request.form['password']
-        rptpassword=request.form['rptpassword']
-        if rptpassword == password :
-            connection= mysql.connect()
-            cur = connection.cursor()
-            cur.execute("INSERT INTO empleado (nombre,correo,contraseña) VALUES (%s,%s,%s)",(name,correo,password))
-            connection.commit()
-            # results = cur.fetchall()
-            return redirect("/")
-        else:
-            return "Error grave"
+        if request.method == "POST":
+            name = request.form['nombre']
+            correo = request.form['correo']
+            password = request.form['password']
+            rptpassword=request.form['rptpassword']
+            if rptpassword == password :
+                connection= mysql.connect()
+                cur = connection.cursor()
+                cur.execute("INSERT INTO empleado (nombre,correo,contraseña) VALUES (%s,%s,%s)",(name,correo,password))
+                connection.commit()
+                return redirect("/")
+            else:
+                return "Error grave"
 
 @app.route("/home")
 def index():
-    return render_template('index.html')
+    if request.method == "GET":
+        connection = mysql.connect()
+        cur = connection.cursor()
+        cur.execute("SELECT * FROM `empleado`")
+        results = cur.fetchall()
+        connection.commit()
+        return render_template("index.html",results=results)
+
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
